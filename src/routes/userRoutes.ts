0ad23be04832b42826b12947587a6657fb699297
VictorDauph <79511345+VictorDauph@users.noreply.github.com>
2025-01-22 16:50:48 +0100

    rangement user routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,14 +1,13 @@
 import { Router } from "express";
-   import { getAllUsers } from "../controllers/userController";
+import { createUser, deleteUser, getAllUsers, updateUser } from "../controllers/userController";
 
-   const router = Router();
+const router = Router();
 
-   router.get('/users', getAllUsers);
+router.get('/users', getAllUsers);
+router.post('/users', createUser);
+router.put('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
 
-   export default router;
-   import { createUser } from "../controllers/userController";
-   router.post('/users', createUser);
-   import { updateUser } from "../controllers/userController";
-   router.put('/users/:id', updateUser);
-   import { deleteUser } from "../controllers/userController";
-   router.delete('/users/:id', deleteUser);
+export default router;
+
+
