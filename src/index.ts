4fb9d2a1ad179ad9692fd76b9d9af533c6373460
VictorDauph <79511345+VictorDauph@users.noreply.github.com>
2025-01-22 16:26:07 +0100

    suppression dashboard routes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import todoRoutes from "./routes/todoRoutes";
 import userRoutes from "./routes/userRoutes";
-import dashboardRoutes from "./routes/dashboardRoutes";
+
 
 const app = express();
 
@@ -10,7 +10,6 @@ app.use(express.json());
 // Routes
 app.use('/api', todoRoutes);
 app.use('/api', userRoutes);
-app.use('/api', dashboardRoutes);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
--- src/routes/dashboardRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getAllDashboard, getUserDashboard } from "../controllers/dashboardController";
-
-const router = Router();
-
-router.get('/dashboard/', getAllDashboard);
-router.get('/dashboard/:userId', getUserDashboard);
-
-export default router;
